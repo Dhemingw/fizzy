1917715c86293997e35710544f6135e4c697b593
Deontre Hemingway <deontrehemingway@Deontres-MacBook-Pro.local>
2020-09-24 14:04:50 -0400

    assignment

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 function fizzy(num) {
-
+  // if num is divisible by 3 return 'Fizz'
+  if (num % 3 === 0) {
+    return 'Fizz'
+  }
+  if (num % 5 === 0) {
+    return 'Buzz'
+  }
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'FizzBuzz'
+  }
 }
 
 module.exports = {
   fizzy,
 }
+
+
+
+
